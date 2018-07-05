83a3e6a834e105dab7873326812a206ce5b949f7
Sam Holmes <samholmes1337@gmail.com>
2018-07-05 14:48:22 +0100

    Use es6 short object notation

--- index.js
+++ index.js
@@ -15,13 +15,13 @@ module.exports = function (content) {
   let component;
 
   svg
-    .optimize(content, { path: path })
+    .optimize(content, { path })
     .then((result) => {
       const compiled = compiler.compile(result.data, { preserveWhitespace: false });
 
       component = transpile(`var render = function () {${compiled.render}};`);
       component += `var toString = function () {return ${JSON.stringify(path)}};`;
-      component += `module.exports = { render: render, toString: toString };`;
+      component += `module.exports = { render, toString };`;
 
       cb(null, component);
     })
