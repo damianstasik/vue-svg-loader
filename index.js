75a31bcbcca4b328d170f5e72da5f898533fc6a1
Acidic <ariseyhun@live.com.au>
2019-04-03 14:57:44 +1030

    Add support for .svgo.yaml config file

--- index.js
+++ index.js
@@ -1,14 +1,25 @@
 const svgToVue = require('svg-to-vue');
 const { getOptions } = require('loader-utils');
+const findUp = require('find-up');
+const yaml = require('js-yaml');
+const fs = require('fs');
 
 module.exports = function (content) {
   const callback = this.async();
   const { svgo } = getOptions(this) || {};
 
-  svgToVue(content, {
-    svgoPath: this.resourcePath,
-    svgoConfig: svgo,
-  })
-    .then(component => callback(null, component))
-    .catch(callback);
+  (async () => {
+	let svgoConfig = {}
+	try {
+		const svgoConfigPath = await findUp('.svgo.yaml')
+		svgoConfig = yaml.safeLoad(fs.readFileSync(svgoConfigPath, 'utf8'))
+	} catch (err) { }
+
+	svgToVue(content, {
+		svgoPath: this.resourcePath,
+		svgoConfig: { ...svgoConfig, ...svgo },
+	})
+	.then(component => callback(null, component))
+	.catch(callback);
+  })()
 };
