2a3b51cc32f146a55b9e601687b5d4ba89116475
Damian Stasik <visualfanatic@users.noreply.github.com>
2021-03-01 22:37:43 +0100

    WIP SVGO 2.x

--- index.js
+++ index.js
@@ -1,28 +1,21 @@
-const SVGO = require('svgo');
+const { optimize } = require('svgo');
 const { getOptions } = require('loader-utils');
 const { version } = require('vue');
 const semverMajor = require('semver/functions/major')
 
-module.exports = async function (svg) {
-  const callback = this.async();
+module.exports = function vueSvgLoader(svg) {
   const { svgo: svgoConfig } = getOptions(this) || {};
 
   if (svgoConfig !== false) {
-    const svgo = new SVGO(svgoConfig);
-
-    try {
-      ({ data: svg } = await svgo.optimize(svg, {
-        path: this.resourcePath,
-      }));
-    } catch (error) {
-      callback(error);
-      return;
-    }
+    ({ data: svg } = optimize(svg, {
+      path: this.resourcePath,
+      ...svgoConfig
+    }));
   }
 
   if (semverMajor(version) === 2) {
     svg = svg.replace('<svg', '<svg v-on="$listeners"');
   }
 
-  callback(null, `<template>${svg}</template>`)
+  return `<template>${svg}</template>`;
 };
