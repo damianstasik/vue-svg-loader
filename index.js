799151d3e3de795f9f3aef2c5f1f4e2bdbbc5395
Andreas Schmidt <andreas.schmidt@gammacommunications.de>
2022-09-26 17:33:33 +0200

    fix: fix template creation, provide sensible defaults to SVGO

--- index.js
+++ index.js
@@ -1,21 +1,47 @@
 const { optimize } = require('svgo');
-const { getOptions } = require('loader-utils');
 const { version } = require('vue');
 const semverMajor = require('semver/functions/major')
 
-module.exports = function vueSvgLoader(svg) {
-  const { svgo: svgoConfig } = getOptions(this) || {};
+const defaultConfig = {
+  plugins: [
+    {
+      name: 'preset-default',
+      params: {
+        overrides: {
+          inlineStyles: {
+            //force deletion of <style> tags in order for vue-loader to accept the template (no nested tags with side effects allowed)
+            onlyMatchedOnce: false
+          }
+        }
+      }
+    }
+  ]
+}
 
-  if (svgoConfig !== false) {
-    ({ data: svg } = optimize(svg, {
-      path: this.resourcePath,
-      ...svgoConfig
-    }));
+module.exports = function vueSvgLoader(svg) {
+  let { svgo: svgoConfig } = this.getOptions() || {};
+
+  // merge svgoConfig with defaultConfig in order to force inlining <style> tags
+  svgoConfig = {...svgoConfig, ...defaultConfig};
+
+  let optimized;
+  optimized = optimize(svg, {
+    path: this.resourcePath,
+    ...svgoConfig
+  });
+
+  if(optimized.modernError ?? optimized.error){
+    let error = optimized.modernError ?? optimized.error;
+    // we'll throw a fatal error here for now
+    // this will stop the current compilation run and make the user aware that at least one of the svgs could not be processed
+    throw error;
   }
+
+  let data = optimized.data;
 
   if (semverMajor(version) === 2) {
-    svg = svg.replace('<svg', '<svg v-on="$listeners"');
+    data = data.replace('<svg', '<svg v-on="$listeners"');
   }
 
-  return `<template>${svg}</template>`;
+  return `<template>${data}</template>`;
 };
